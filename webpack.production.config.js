2e51151087663ff006ef63b5b8e34f65ce7b3688
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2019-01-26 09:35:00 +0400

    Added assets minification for production

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,14 +2,14 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 
 const base = require('./webpack.base.config');
 
 module.exports = merge(base, {
   mode: 'production',
-
-  devtool: 'source-map',
 
   module: {
     rules: [
@@ -24,6 +24,23 @@ module.exports = merge(base, {
     ],
   },
 
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+      new OptimizeCssAssetsPlugin({
+        cssProcessorPluginOptions: {
+          preset: ['default', { discardComments: { removeAll: true } }],
+        },
+      }),
+    ],
+  },
+
   plugins: [
     new webpack.DefinePlugin({
       NODE_ENV: 'production',
@@ -35,8 +52,6 @@ module.exports = merge(base, {
     new MiniCssExtractPlugin({
       filename: 'assets/[name].[hash].css',
     }),
-    new ManifestPlugin({
-
-    }),
+    new ManifestPlugin(),
   ],
 });
